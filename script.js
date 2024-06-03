57d4bdbc1e0dfb1ad1470b6b14caf184d2e0211c
ShahHassanAfridi <shahhassan.hp78@gmail.com>
2024-06-03 12:21:25 -0700

    JS if if-else mini projects

---
+++ script.js
@@ -0,0 +1,99 @@
+//BRT CARD BALANCE
+
+// var cardBalance = +prompt("Enter your Card Balance 🤨:")
+
+// if(cardBalance >= 50){
+//     console.log("Welcome to Zu Peshawar 😀")
+// }
+// else{
+//     console.log("Please Recharge your Zu Card 😓")
+// }
+
+//SHOPPING DISCOUNT
+
+// var shoppingDiscount = +prompt("Your total amount of shopping:")
+
+// if(shoppingDiscount <= 4999){
+//     console.log("You've got 10% discount")
+// }
+// else if (shoppingDiscount <= 9999){
+//     console.log("You've got 25% discount")
+// }
+// else if (shoppingDiscount >= 10000){
+//     console.log("You've got 50% discount")
+// }
+
+//TRAFFIC LIGHT
+
+// var trafficLight = prompt("Traffic Light Color:")
+
+// if(trafficLight === "Red"){
+//     console.log("STOP")
+// }
+// else if(trafficLight === "Yellow"){
+//     console.log("Get Ready")
+// }
+// else if(trafficLight === "Green"){
+//     console.log("Go")
+// }
+// else{
+//     console.log("Invalid")
+// }
+
+//BARISH
+
+// var rainExpected = prompt("Badal hai kiya")
+
+// if(rainExpected === "Yes"){
+//     console.log("Aj barish hogi")
+// }
+// else if(rainExpected === "No") {
+//     console.log("Aj barish nahi hogi")
+// }
+// else{
+//     console.log("Muje nahi pata")
+// }
+
+//ATM BALANCE
+
+// var Withdrawal = +prompt("How much do you want to Withdraw:")
+// var bankBalance = 5000;
+
+// if(Withdrawal <= bankBalance){
+//      console.log("withrawal successful")
+// }
+// else{
+//     console.log("Insufficient balance")
+// }
+
+// var burger = 350;
+// var jebMePaise = +prompt("How much money do you have")
+
+// if(jebMePaise >= burger){
+//     console.log("Lele burger")
+// }
+// else{
+//     console.log("Patli gali se nikal")
+// }
+
+//EVEN-ODD NUMBERS
+
+// var number = +prompt("Enter a number")
+
+// if (number % 2 === 0) {
+//     console.log("The number is Even")
+// }
+// else{
+//     console.log("The number is Odd")
+// }
+
+//FRUITS TOTAL  BILL
+
+// var applePrice = +prompt("Enter price of apple")
+// var bananaPrice = +prompt("Enter the price of banana")
+// var pearPrice = +prompt("Enter the price of pear")
+// var watermelonPrice = +prompt("Enter the price of watermelon")
+
+// var totalBill = applePrice + bananaPrice + pearPrice + watermelonPrice;
+// console.log(`Total Bill is: ${totalBill}`)
+
